f3163523ccc6042d6f9b83af8ef02966c51629b7
August McAllister <august.chm@gmail.com>
2023-03-15 13:42:39 -0500

    passing recipes as props to pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,16 @@
-"use server";
 import { Recipe } from "@prisma/client";
 import { getRecipes, getRecipeById } from "@api/recipe";
-import RecipeSearch from "./recipe-search";
-import { CLIENT_STATIC_FILES_RUNTIME_WEBPACK } from "next/dist/shared/lib/constants";
-
-const recipeSearch = async (searchId: number) => {
-  const recipe = await getRecipeById(searchId)
-  return recipe ? recipe.json() : 'No recipe found'
-}
+import RecipeDisplay from "@comp/RecipeDisplay";
 
 export default async function Home() {
   const recipes = await getRecipes();
 
-  const RecipeSearchButton = () => {
-    return (
-      <>
-        <button>yeah</button>
-      </>
-    );
-  };
-
   return (
     <main>
-      {/* {recipes.map((recipe: Recipe) => (
-        <div key={recipe.id}>
-          <h1>{recipe.title}</h1>
-          <p>Cook time: {recipe.cookTime}</p>
-        </div>
-      )) } */}
 
       <div>
-        <RecipeSearch />
+        <RecipeDisplay recipes={recipes} />
       </div>
-      <div></div>
     </main>
   );
 }
