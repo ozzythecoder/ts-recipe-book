0d36337f88ee887f4ea553128c04fc35ee9e979e
August McAllister <august.chm@gmail.com>
2023-03-15 11:11:03 -0500

    Testing different methods to pass interactivity to client

--- app/api/recipe/index.ts
+++ app/api/recipe/index.ts
@@ -1,6 +1,21 @@
-import { GET } from "./route"
+import { db } from "@/lib/db";
+import { GET as fetchRecipes } from "./route"
+import { GET as fetchRecipeById } from "./[id]/route";
 
-export const getRecipes = async () => {
-  const recipes = await GET();
-  return recipes.json();
+const getRecipes = async () => {
+  const data = await fetchRecipes();
+  return data.json();
 }
+
+const getRecipeById = async(id: number) => {
+  
+  const recipe = await db.recipe.findUnique({
+    where: {
+      id: id
+    }
+  })
+
+  return recipe;
+}
+
+export { getRecipes, getRecipeById };
