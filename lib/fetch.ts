249ff4d77708137d79aa1a8243bb2b329809ea91
August McAllister <august.chm@gmail.com>
2023-03-15 15:01:04 -0500

    refactor: moved API helper functions to lib directory

---
+++ lib/fetch.ts
@@ -0,0 +1,13 @@
+
+
+export const getRecipes = async () => {
+  const dataRes = await fetch(`http://localhost:3000/api/recipe`, { method: 'GET' })
+  const data = await dataRes.json()
+  return data;
+}
+
+export const getRecipesById = async (searchIdIn: number) => {
+  const dataRes = await fetch(`http://localhost:3000/api/recipe/${searchIdIn}`, { method: "GET" })
+  const data = await dataRes.json()
+  return data;
+}
