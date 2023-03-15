20d50d7da6aee10e9469077dc395ff85e913e256
August McAllister <august.chm@gmail.com>
2023-03-15 11:11:26 -0500

    Testing different methods to pass interactivity to client

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,38 @@
-import { Recipe } from "@prisma/client"
-import { getRecipes } from "@api/recipe";
+"use server";
+import { Recipe } from "@prisma/client";
+import { getRecipes, getRecipeById } from "@api/recipe";
+import RecipeSearch from "./recipe-search";
+import { CLIENT_STATIC_FILES_RUNTIME_WEBPACK } from "next/dist/shared/lib/constants";
+
+const recipeSearch = async (searchId: number) => {
+  const recipe = await getRecipeById(searchId)
+  return recipe ? recipe.json() : 'No recipe found'
+}
 
 export default async function Home() {
+  const recipes = await getRecipes();
 
-  const { data } = await getRecipes();
+  const RecipeSearchButton = () => {
+    return (
+      <>
+        <button>yeah</button>
+      </>
+    );
+  };
 
   return (
     <main>
-      {data.map((recipe: Recipe) => (
+      {/* {recipes.map((recipe: Recipe) => (
         <div key={recipe.id}>
           <h1>{recipe.title}</h1>
           <p>Cook time: {recipe.cookTime}</p>
         </div>
-      )) }
+      )) } */}
+
+      <div>
+        <RecipeSearch />
+      </div>
+      <div></div>
     </main>
-  )
+  );
 }
