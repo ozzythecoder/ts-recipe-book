de92bee9cf5b2843db3b78c22704f5b4f6602158
August McAllister <august.chm@gmail.com>
2023-03-17 12:32:59 -0500

    feat: updated recipedisplay to use new UI components

--- app/components/RecipeDisplay/index.tsx
+++ app/components/RecipeDisplay/index.tsx
@@ -3,40 +3,22 @@ import { Recipe } from "@prisma/client";
 import { useState } from "react";
 import { getRecipesById } from "@/lib/fetch";
 
+import CardTable from "@ui/CardTable";
+import RecipePreview from "../RecipePreview";
+
 interface Props extends React.PropsWithChildren {
   recipes?: Recipe[];
 }
 
 function RecipeDisplay({ recipes, children }: Props) {
-  const [searchIdIn, setSearchId] = useState<string>("");
-  const [stuff, setStuff] = useState<any>();
-
-  const handleFetch = async () => {
-    try {
-      const data = await getRecipesById(searchIdIn);
-      data ? setStuff(JSON.stringify(data)) : setStuff("Nothing found");
-    } catch (e) {
-      console.log(e);
-      setStuff("Error");
-    }
-  };
 
   return (
     <>
-      {recipes?.map((recipe: Recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>Prep time: {recipe.prepTime}</p>
-          <p>Cook time: {recipe.cookTime}</p>
-        </div>
-      ))}
-      <input
-        type="text"
-        value={searchIdIn}
-        onChange={(e) => setSearchId(e.target.value)}
-      />
-      <button onClick={handleFetch}>Get recipe data</button>
-      {stuff}
+      <CardTable>
+        {recipes?.map((recipe: Recipe) => (
+          <RecipePreview key={recipe.id} recipe={recipe} />
+        ))}
+      </CardTable>
     </>
   );
 }
