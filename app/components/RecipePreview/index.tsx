c623a3f35a827475f5dfc9a14b783fdc1f64cce6
August McAllister <august.chm@gmail.com>
2023-03-17 12:30:00 -0500

    refactor: moved recipe preview card into new component

---
+++ app/components/RecipePreview/index.tsx
@@ -0,0 +1,17 @@
+import { Recipe } from "@prisma/client";
+import Card from "@ui/Card";
+
+interface Props extends React.PropsWithChildren {
+  recipe: Recipe
+}
+
+export default function RecipePreview({ recipe }: Props) {
+  
+  return (
+    <Card key={recipe.id}>
+      <h3>{recipe.title}</h3>
+      <p>Prep time: {recipe.prepTime}</p>
+      <p>Cook time: {recipe.cookTime}</p>
+    </Card>
+  );
+}
