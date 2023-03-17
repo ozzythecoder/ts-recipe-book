651c0542fef27a8417d7a109660046ed0ea792c3
August McAllister <august.chm@gmail.com>
2023-03-17 12:28:21 -0500

    fix: updated fetch to not use old data from cache

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,13 +1,17 @@
 
 
 export const getRecipes = async () => {
-  const dataRes = await fetch(`http://localhost:3000/api/recipe`, { method: 'GET' })
-  const data = await dataRes.json()
-  return data;
+  try {
+    const dataRes = await fetch(`http://localhost:3000/api/recipe`, { method: 'GET', cache: "no-store" })
+    const data = await dataRes.json()
+    return data;
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export const getRecipesById = async (searchIdIn: string) => {
-  const dataRes = await fetch(`http://localhost:3000/api/recipe/${searchIdIn}`, { method: "GET" })
+  const dataRes = await fetch(`http://localhost:3000/api/recipe/${searchIdIn}`, { method: "GET", cache: "no-store" })
   const data = await dataRes.json()
   return data;
 }
