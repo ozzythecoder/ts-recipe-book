14021d2d44a32dc969d1b7e996532a009bc7cfad
August McAllister <august.chm@gmail.com>
2023-03-20 09:12:36 -0500

    layout on recipe details page

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
 
+import { getRecipes } from "@/lib/fetch";
+import { Recipe } from "@prisma/client";
+import RecipePreview from "../components/RecipePreview";
+import CardTable from "../components/ui/CardTable";
 
-export default function Recipes() {
+export default async function Recipes() {
+  const recipes: Recipe[] = await getRecipes();
 
   return (
-    <div>
-      Recipes
-    </div>
-  )
+    <main>
+      <CardTable>
+        {recipes.map((recipe: Recipe) => (
+          <Link href={`/recipes/${recipe.id}`} key={recipe.id}>
+            <RecipePreview recipe={recipe} />
+          </Link>
+        ))}
+      </CardTable>
+    </main>
+  );
 }
