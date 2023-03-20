1db2a832c155cda80e08a0dcbedaa44029ca6b3a
August McAllister <august.chm@gmail.com>
2023-03-20 10:59:28 -0500

    added test API post route

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -3,5 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function GET(request?: Request) {
   const data = await db.recipe.findMany();
-  return NextResponse.json( data )
+  return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+  // test route
+  const data = await request.json();
+  console.log(data);
+  return;
 }
