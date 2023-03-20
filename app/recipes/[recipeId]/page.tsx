0e230fcbaad21c4a02a96199a2f82fad5ba694af
August McAllister <august.chm@gmail.com>
2023-03-20 09:12:25 -0500

    card styling on recipe overview

--- app/recipes/[recipeId]/page.tsx
+++ app/recipes/[recipeId]/page.tsx
@@ -1,23 +1,38 @@
+import Link from "next/link";
+
 import { getRecipesById } from "@/lib/fetch";
 import { Recipe } from "@prisma/client";
+import Card from "@ui/Card";
 
 interface Props extends React.PropsWithChildren {
-  recipe: Recipe
   params: {
     recipeId: string;
-  }
+  };
 }
 
-export default async function RecipeDetail ({ params }: Props) {
-  
+export default async function RecipeDetail({ params }: Props) {
   const { recipeId } = params;
 
-  const recipe = await getRecipesById(recipeId)
+  const recipe: Recipe = await getRecipesById(recipeId);
 
   return (
-    <div>
-      {JSON.stringify(recipe)}
-    </div>
-  )
-
+    <section
+      style={{ margin: '20px auto', textAlign: 'center' }}
+    >
+      <Card
+        style={{ margin: '0 auto', minWidth: '40%', textAlign: 'initial' }}
+      >
+        <h1>{recipe.title}</h1>
+        <p>Rating: {recipe.rating} / 5</p>
+        <p>Prep Time: {recipe.prepTime} minutes</p>
+        <p>Cook Time: {recipe.cookTime} minutes</p>
+        <ol>
+          {recipe.instructions.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ol>
+      </Card>
+      <Link href="/recipes">Back to recipes</Link>
+    </section>
+  );
 }
