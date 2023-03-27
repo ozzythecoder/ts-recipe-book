a4abb616539f689457e7923139f9c894fd8f6dcf
August McAllister <august.chm@gmail.com>
2023-03-27 14:46:43 -0500

    updated form and fetch parameters

--- app/components/AddRecipeForm/index.tsx
+++ app/components/AddRecipeForm/index.tsx
@@ -1,34 +1,90 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 import Button from "@ui/Button";
 import Input from "@ui/Input";
+import Autocomplete from "@ui/Autocomplete";
+
+import { Ingredient } from "@prisma/client";
+import { DetailedIngredient } from "@/lib/types";
 
 import styles from "./style.module.css";
+import IngredientAddition from "./IngredientAddition";
 
 interface Props extends React.PropsWithChildren {
-  url: string;
+  ingredientInitial: DetailedIngredient[];
 }
 
-export default function AddRecipeForm({ url, children }: Props) {
+export default function AddRecipeForm({ ingredientInitial, children }: Props) {
   const [titleIn, setTitle] = useState<string>("");
-  const [ratingIn, setRating] = useState<number>(0);
-  const [prepTimeIn, setPrepTime] = useState<number>(0);
-  const [cookTimeIn, setCookTime] = useState<number>(0);
+  const [ratingIn, setRating] = useState<string>("");
+  const [prepTimeIn, setPrepTime] = useState<string>("");
+  const [cookTimeIn, setCookTime] = useState<string>("");
+  const [ingredientsIn, setIngredientsIn] = useState<DetailedIngredient[]>([]);
+  const [ingredientOptions, setIngredientOptions] =
+    useState<DetailedIngredient[]>(ingredientInitial);
+
+  const updateIngredientOptions = () => {
+    setIngredientOptions(
+      ingredientInitial.filter((item) => {
+        if (ingredientsIn.map((ing) => ing.id).includes(item.id)) return false;
+        return true;
+      })
+    );
+  };
+
+  const addIngredient = (newIngredient: Ingredient) => {
+    setIngredientsIn([
+      ...ingredientsIn,
+      { ...newIngredient, amount: "0", unit: "" },
+    ]);
+  };
+
+  const changeAmount = (id: string, newAmount: string) => {
+    setIngredientsIn(
+      ingredientsIn.map((item) => {
+        return item.id === parseInt(id) ? { ...item, amount: newAmount } : item;
+      })
+    );
+  };
+
+  const changeUnit = (id: string, newUnit: string) => {
+    setIngredientsIn(
+      ingredientsIn.map((item) => {
+        return item.id === parseInt(id) ? { ...item, unit: newUnit } : item;
+      })
+    );
+  };
+
+  const removeIngredient = (badIngredient: DetailedIngredient) => {
+    setIngredientsIn(
+      ingredientsIn.filter((item) => item.id !== badIngredient.id)
+    );
+  };
+
+  useEffect(updateIngredientOptions, [ingredientsIn]);
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    await fetch(url, {
-      body: JSON.stringify({
-        title: titleIn,
-        rating: ratingIn,
-        prepTime: prepTimeIn,
-        cookTime: cookTimeIn,
-      }),
+    const recipeItem = {
+      title: titleIn,
+      rating: ratingIn,
+      prepTime: prepTimeIn,
+      cookTime: cookTimeIn,
+      ingredients: ingredientsIn
+    }
+
+    await fetch("http://localhost:3000/api/recipe", {
+      body: JSON.stringify(
+        recipeItem
+      ),
       headers: { "Content-Type": "application/json" },
       method: "POST",
     });
+
+    console.log(recipeItem)
   };
 
   return (
@@ -50,7 +106,7 @@ export default function AddRecipeForm({ url, children }: Props) {
           min={0}
           max={5}
           value={ratingIn}
-          onChange={(e) => setRating(parseInt(e.target.value))}
+          onChange={(e) => setRating(e.target.value)}
         />
       </label>
       <label htmlFor="">
@@ -59,7 +115,7 @@ export default function AddRecipeForm({ url, children }: Props) {
           className={styles.formItem}
           type="number"
           value={prepTimeIn}
-          onChange={(e) => setPrepTime(parseInt(e.target.value))}
+          onChange={(e) => setPrepTime(e.target.value)}
         />
       </label>
       <label htmlFor="">
