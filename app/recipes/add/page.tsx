d9ac5e081b84afe772aa1480bfb1e8cdf4efaec0
August McAllister <august.chm@gmail.com>
2023-03-27 14:49:51 -0500

    feat: added async fetch wrapper for recipe form

--- app/recipes/add/page.tsx
+++ app/recipes/add/page.tsx
@@ -2,12 +2,15 @@ import Link from "next/link";
 
 import AddRecipeForm from "@comp/AddRecipeForm";
 
-export default function RecipeAdd() {
+export default async function RecipeAdd() {
+
+  const res = await fetch('http://localhost:3000/api/ingredient')
+  const ingredientInitial = await res.json()
 
   return (
     <main>
       <AddRecipeForm
-        url="http://localhost:3000/api/recipe"
+        ingredientInitial={ingredientInitial}
       />
       <Link href="/recipes">Go back</Link>
     </main>
