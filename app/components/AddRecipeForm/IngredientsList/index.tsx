d9c7adca4f36a389a15dbd5195b9f34512a41ac4
August McAllister <august.chm@gmail.com>
2023-03-30 16:47:18 -0500

    removed the cruft from ingredient list component

--- app/components/AddRecipeForm/IngredientsList/index.tsx
+++ app/components/AddRecipeForm/IngredientsList/index.tsx
@@ -1,2 +1,21 @@
-import { useEffect, useState } from "react";
-import { useReducer, useContext } from "react";
+import { useContext } from "react";
+
+import { FormContext } from "../FormContext";
+import UList from "@ui/UList";
+import IngredientAddition from "../IngredientAddition";
+
+export default function IngredientsList() {
+
+  const { ingredientsIn } = useContext(FormContext)
+
+  return ingredientsIn ? (
+    <UList>
+      {ingredientsIn.map((item) => (
+        <IngredientAddition
+          key={item.id}
+          ingredient={item}
+        />
+      ))}
+    </UList>
+  ) : null;
+};
