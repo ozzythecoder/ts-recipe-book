c33cd35bf4d32ad3b93d4042188207f4a7df1e05
August McAllister <august.chm@gmail.com>
2023-04-02 13:08:19 -0500

    Adjusted styling of custom UI components

--- app/components/ui/Button/index.tsx
+++ app/components/ui/Button/index.tsx
@@ -6,6 +6,15 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export default function Button({ style, callback, children, ...props }: Props) {
 
   const buttonStyle: React.CSSProperties = {
+    fontSize: "1rem",
+    textTransform: "uppercase",
+    padding: "10px 20px",
+    margin: "0.1rem 0.5rem",
+    backgroundColor: "#0000ffbb",
+    color: "white",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
     ...style
   }
 
--- app/components/ui/Input/index.tsx
+++ app/components/ui/Input/index.tsx
@@ -4,6 +4,9 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 export default function Input({ style, ...props }: Props) {
   
   const inputStyle: React.CSSProperties = {
+    height: "calc(1rem + 10px)",
+    fontSize: "1.2rem",
+    margin: "5px",
     ...style
   }
 
