f37a7fd1e5ba12cddf447cc0fd3f1e45d4e191b7
August McAllister <august.chm@gmail.com>
2023-04-02 15:05:03 -0500

    Renamed stylesheet for consistency; added wrapper component for card table

--- app/components/ui/CardTable/index.tsx
+++ app/components/ui/CardTable/index.tsx
@@ -1,14 +1,18 @@
-import styles from './style.module.css';
+import clsx from 'clsx';
+import './styles.css'
 
 interface Props extends React.PropsWithChildren {
   style? : React.CSSProperties;
+  className? : string;
 }
 
-export default function CardTable({ children, style }: Props) {
+export default function CardTable({ children, className, style }: Props) {
 
   return (
-    <section style={style} className={styles.cardTable}>
-      {children}
+    <section style={style} className={className}>
+      <div className="card-table">
+        {children}
+      </div>
     </section>
   )
 }
