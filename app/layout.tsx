250301cde5c678a34f38949ad3b2f6e53485b562
August McAllister <august.chm@gmail.com>
2023-04-18 16:21:02 -0500

    New layout & page files

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function App({ children }: { children: React.ReactNode }) {
+
+  return (
+    <html lang="en">
+      <head>
+        <title>Recipe Book</title>
+      </head>
+      <body>
+        { children }
+      </body>
+    </html>
+  )
+}
