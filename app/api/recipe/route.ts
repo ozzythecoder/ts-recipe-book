d87cb0b7c97dd0466bf10a27c447fb1ce5580deb
August McAllister <august.chm@gmail.com>
2023-04-24 17:21:02 -0500

    added error handling to GET; added test POST request

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -1,7 +1,23 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await db.recipe.findMany();
-  return NextResponse.json(res);
+export async function GET(
+  request: Request,
+  { searchParams }: { searchParams: { name?: string } }
+  ) {
+
+  try {
+    
+    const res = await db.recipe.findMany();
+    return NextResponse.json(res);
+  } catch (e) {
+    return NextResponse.json({ "message": "There was an oopsie" })
+  }
+}
+
+export async function POST(request: Request) {
+  
+  const body = await request.json();
+
+  return NextResponse.json({ "message": `You sent: ${JSON.stringify(body)}`})
 }
