dab4da4229133137233616e41c4d1507f0efe85b
August McAllister <august.chm@gmail.com>
2023-04-24 18:27:48 -0500

    added search by name to ingredient endpoint

--- app/api/ingredient/route.ts
+++ app/api/ingredient/route.ts
@@ -1,7 +1,35 @@
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await db.ingredient.findMany();
-  return NextResponse.json(res);
+export async function GET(
+  request: NextRequest
+) {
+
+  let params: any = {};
+  for (const [key, val] of request.nextUrl.searchParams.entries()) {
+    params[key] = val;
+  }
+  const { name, id } = params;
+
+  try {
+    let res;
+
+    if (name) {
+      res = await db.ingredient.findMany({
+        where: {
+          name: {
+            contains: name,
+            mode: 'insensitive'
+          }
+        }
+      })
+    } else {
+      res = await db.ingredient.findMany();
+    }
+
+    return NextResponse.json(res);
+  } catch (e) {
+    return NextResponse.json({ "message": "We did a whoopsie" })
+  }
+
 }
