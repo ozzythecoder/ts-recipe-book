3b74ba445dda9ff251ab6887ddc37dcdd9859e9d
August McAllister <august.chm@gmail.com>
2023-04-24 17:20:02 -0500

    set up search input

---
+++ lib/components/SearchField/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useSearchContext } from "@/lib/context/search";
+
+const SearchField = () => {
+  const { recipeSearchIn, setRecipeSearch } = useSearchContext();
+
+  const handleChange = (e: React.SyntheticEvent) => {
+    if (setRecipeSearch) {
+      setRecipeSearch((e.target as HTMLInputElement).value);
+    }
+  }
+
+  return (
+    <>
+      <input type="text" value={recipeSearchIn} onChange={handleChange} />
+      <span>{recipeSearchIn}</span>
+    </>
+  );
+};
+
+export default SearchField;
