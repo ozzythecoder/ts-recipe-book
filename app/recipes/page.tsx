69a49eff15e2af4e55e15196cbf9d3abd137268c
August McAllister <august.chm@gmail.com>
2023-05-30 15:55:13 -0500

    Testing out Suspense method

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { db } from "@/lib/db";
 import RecipePreview from "@components/RecipePreview";
 import { SearchContextProvider } from "@/lib/context/search";
@@ -5,18 +6,16 @@ import SearchField from "@components/SearchField";
 import RecipeList from "@components/RecipeList";
 
 export default async function RecipeView() {
-  const recipes = await db.recipe.findMany();
 
-
+  const allRecipes = await db.recipe.findMany();
 
   return (
     <>
       <SearchContextProvider>
         <SearchField />
-        <RecipeList />
-        {recipes.map((recipe) => (
-          <RecipePreview recipe={recipe} />
-        ))}
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <RecipeList allRecipes={allRecipes} />
+        </Suspense>
       </SearchContextProvider>
     </>
   );
