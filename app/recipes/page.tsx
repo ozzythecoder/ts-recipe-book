0eafdcd4b38c0b395865c543ce0b6e84e4513197
August McAllister <august.chm@gmail.com>
2023-05-31 17:21:15 -0500

    Moved fetch logic into timeout function to mock network latency

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { db } from "@/lib/db";
+import type { Recipe } from "@prisma/client";
 import RecipePreview from "@components/RecipePreview";
 import { SearchContextProvider } from "@/lib/context/search";
 import SearchField from "@components/SearchField";
@@ -7,15 +8,20 @@ import RecipeList from "@components/RecipeList";
 
 export default async function RecipeView() {
 
-  const allRecipes = await db.recipe.findMany();
+  // Fetch recipes and fake network latency
+  const getRecipes = async (): Promise<Recipe[]> => {
+    const res = await db.recipe.findMany();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return res;
+  };
+
+  const allRecipes = await getRecipes();
 
   return (
     <>
       <SearchContextProvider>
         <SearchField />
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <RecipeList allRecipes={allRecipes} />
-        </Suspense>
+        <RecipeList allRecipes={allRecipes} />
       </SearchContextProvider>
     </>
   );
