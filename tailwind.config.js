b222b60958dc229829cf6875049e765992bc994b
August McAllister <august.chm@gmail.com>
2023-05-31 17:30:36 -0500

    Updated dependencies: added tailwind & updated to next 13.4 (stable version)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{tsx}",
+    "./lib/**/*.{tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
