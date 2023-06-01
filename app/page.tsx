42a8585dd07387316e7f211318966b334285f21b
August McAllister <august.chm@gmail.com>
2023-06-01 12:51:14 -0500

    Added styling to home page link

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello world!</h1>
-      <Link href="/recipes">Let's eat!</Link>
+      <Link className="text-gray-400 hover:text-black hover:underline focus:underline" href="/recipes">Let's eat!</Link>
     </main>
   );
 }
