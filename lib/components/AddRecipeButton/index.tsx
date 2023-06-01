9350d5534ac3f43a568e7c7e3a9b12f927178e67
August McAllister <august.chm@gmail.com>
2023-06-01 12:15:59 -0500

    Added Add Recipe button component

---
+++ lib/components/AddRecipeButton/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Button from "@ui/Button";
+import { useRouter } from "next/navigation";
+
+interface Props extends React.PropsWithChildren {}
+
+export default function AddRecipeButton({}: Props) {
+
+  const router = useRouter();
+
+  const handleClick = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    router.push('/recipes/add');
+  }
+
+  return <Button onClick={handleClick}>Add Recipe</Button>;
+}
