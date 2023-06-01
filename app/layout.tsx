9d3b2978cb08bf0476756c1a8cecc081f57ee668
August McAllister <august.chm@gmail.com>
2023-06-01 12:17:24 -0500

    Added sidebar to standard page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,28 @@
 import "./globals.css";
 
 export default function App({ children }: { children: React.ReactNode }) {
-
   return (
     <html lang="en">
       <head>
         <title>Recipe Book</title>
       </head>
       <body>
-        { children }
+        <div className="mx-4 md:mx-16 mt-4">
+          <h1 className="text-4xl font-bold">Recipe Book</h1>
+          <div className="flex flex-col md:flex-row mt-6">
+            {/* sidebar */}
+            <div className="md:w-1/3 lg:w-1/4">
+              <p>Sidebar stuff goes here</p>
+              <p>Maybe some nav</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            {/* main content */}
+            <div className="md:w-2/3 lg:w-3/4">
+              {children}
+            </div>
+          </div>
+        </div>
       </body>
     </html>
-  )
+  );
 }
