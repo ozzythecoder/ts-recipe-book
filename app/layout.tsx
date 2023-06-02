fe76eeac1e72134edd0de16ed41a8f72d93593e9
August McAllister <august.chm@gmail.com>
2023-06-01 19:38:13 -0500

    adjusted styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,13 +11,13 @@ export default function App({ children }: { children: React.ReactNode }) {
           <h1 className="text-4xl font-bold">Recipe Book</h1>
           <div className="flex flex-col md:flex-row mt-6">
             {/* sidebar */}
-            <div className="md:w-1/3 lg:w-1/4">
+            <div className="md:w-1/3 xl:w-1/5">
               <p>Sidebar stuff goes here</p>
               <p>Maybe some nav</p>
               <p>Lorem ipsum dolor sit amet.</p>
             </div>
             {/* main content */}
-            <div className="md:w-2/3 lg:w-3/4">
+            <div className="md:w-2/3 xl:w-3/5">
               {children}
             </div>
           </div>
