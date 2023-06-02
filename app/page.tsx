0b5a6c8eda12cf748ff71fc3aff5764f4ef7f074
August McAllister <august.chm@gmail.com>
2023-06-02 16:31:24 -0500

    tailwind-ify

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Hello world!</h1>
+      <h1 className="text-3xl">Hello world!</h1>
       <Link className="text-gray-400 hover:text-black hover:underline focus:underline" href="/recipes">Let's eat!</Link>
     </main>
   );
