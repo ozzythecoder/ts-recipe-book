d2ba061ac42c6f79bd3100b26eb9ea7acc01c744
August McAllister <august.chm@gmail.com>
2023-06-01 19:39:19 -0500

    added ingredient display to recipe content

--- lib/components/RecipeContent/index.tsx
+++ lib/components/RecipeContent/index.tsx
@@ -1,7 +1,8 @@
 import { Recipe } from "@prisma/client";
+import { RecipeWithIngredientNames } from "@/lib/types";
 
 interface Props extends React.PropsWithChildren {
-  recipe: Recipe;
+  recipe: RecipeWithIngredientNames;
 }
 
 export default function RecipeContent({ recipe }: Props) {
@@ -12,18 +13,29 @@ export default function RecipeContent({ recipe }: Props) {
       <p>Prep time: {recipe.prepTime} minutes</p>
       <p>Cook time: {recipe.cookTime} minutes</p>
       <div className="divide-y divide-dashed divide-gray-300">
-        <div className="pb-2"></div>
-        <div>
-          <p className="text-bold">Ingredients:</p>
+        <div className="pb-2" />
+        <div className="py-2">
+          <p className="font-bold">Ingredients:</p>
           <ul className="list-disc list-inside">
-
+            {recipe.ingredients.map((ingredient) => (
+              <li>
+                {`
+                  ${ingredient.amount}
+                  ${ingredient.unit === "each" ? "" : ingredient.unit}
+                  ${ingredient.ingredient.name}
+                `}
+              </li>
+            ))}
           </ul>
         </div>
-        <ol className="pt-2 list-decimal list-inside">
-          {recipe.instructions?.map((step) => (
-            <li>{step}</li>
-          ))}
-        </ol>
+        <div className="pt-2">
+          <p className="font-bold">Instructions:</p>
+          <ol className="list-decimal list-inside">
+            {recipe.instructions?.map((step) => (
+              <li>{step}</li>
+            ))}
+          </ol>
+        </div>
       </div>
     </section>
   );
