40e4610ee1efd0960a39f91f3c256554dc55bf73
August McAllister <august.chm@gmail.com>
2023-06-07 22:53:59 -0500

    Installed shadcnui input & label, and adjusted styling

--- lib/components/ui/Input/index.tsx
+++
@@ -1,5 +0,0 @@
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-export default function Input({  }: Props) {
-  return <input className="p-2 my-2 border-2 rounded-sm" />
-}
