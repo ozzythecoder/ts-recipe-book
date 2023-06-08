960504dd6e4d2fea2613fe402f1e610e9ec07f27
August McAllister <august.chm@gmail.com>
2023-06-07 21:33:27 -0500

    Fleshing out form with react-hook-form

--- lib/components/AddRecipeForm/index.tsx
+++ lib/components/AddRecipeForm/index.tsx
@@ -1,32 +1,93 @@
 "use client";
 import { useForm } from "react-hook-form";
+import clsx from "clsx";
 import Input from "@ui/Input";
+import { Slider } from "@ui/slider";
 import Button from "@ui/Button";
+import type { Recipe } from "@prisma/client";
 
 interface Props {}
+
+interface FormData extends Recipe {}
+
+const inputClasses = "border-solid border-2 p-2 rounded-md w-full mobile:w-3/5";
+const inputErrorClasses = "border-red-500 focus:outline-red-500";
 
 export default function AddRecipeForm({}: Props) {
   const {
     register,
     handleSubmit,
-    watch,
+    control,
     formState: { errors },
-  } = useForm();
+  } = useForm<FormData>({
+    defaultValues: {
+      title: "",
+      cookTime: null,
+      prepTime: null,
+    },
+  });
 
   const onSubmit = (data: any) => console.log(data);
 
   //TODO Learn React Hook Form
-  
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input defaultValue="test" {...register("example")} />
+    <>
+      <form
+        className="flex flex-col items-center gap-4 outline outline-1"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          className={clsx(inputClasses, errors.title && inputErrorClasses)}
+          id="title"
+          aria-invalid={!!errors.title}
+          {...register("title", { required: "Required" })}
+          placeholder="Recipe Name"
+        />
+        {errors.title?.message && (
+          <span role="alert" className="text-red-500">{errors.title.message}</span>
+        )}
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <Input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+        <input
+          type="number"
+          id="prep-time"
+          aria-invalid={!!errors.prepTime}
+          className={clsx(inputClasses, errors.prepTime && inputErrorClasses)}
+          {...register("prepTime", {
+            required: "Required",
+            maxLength: {
+              value: 3,
+              message: "Cook time must be 3 digits or less",
+            },
+          })}
+          placeholder="Prep Time"
+        />
+        {errors.prepTime?.message && (
+          <span role="alert" className="text-red-500">{errors.prepTime.message}</span>
+        )}
 
-      <Button type="submit" onClick={e => e.preventDefault()}>Submit</Button>
-    </form>
+        <input
+          type="number"
+          id="cook-time"
+          aria-invalid={!!errors.cookTime}
+          className={clsx(inputClasses, errors.cookTime && inputErrorClasses)}
+          {...register("cookTime", {
+            required: "Required",
+            maxLength: {
+              value: 3,
+              message: "Cook time must be 3 digits or less",
+            },
+          })}
+          placeholder="Cook Time"
+        />
+        {errors.cookTime?.message && (
+          <span role="alert" className="text-red-500">{errors.cookTime.message}</span>
+        )}
+
+        <Slider />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
   );
 }
