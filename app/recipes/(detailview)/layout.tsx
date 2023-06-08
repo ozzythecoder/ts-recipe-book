980078c9f9ca9fb87294104e5bae591f4ff0493f
August McAllister <august.chm@gmail.com>
2023-06-07 22:55:50 -0500

    Fleshed out forms with classes and aria labels; added slider for rating

--- app/recipes/(detailview)/layout.tsx
+++ app/recipes/(detailview)/layout.tsx
@@ -6,7 +6,7 @@ export default function RecipeLayout({ children }: Props) {
   return (
     <>
     <div className="my-2">
-      <Link href="/recipes" className="text-gray-400 hover:text-black hover:underline focus:underline">Back to recipes</Link>
+      <Link href="/recipes" className="text-sm text-gray-400 hover:text-black hover:underline focus:underline">Back to recipes</Link>
     </div>
       <>{children}</>
     </>
--- lib/components/AddRecipeForm/index.tsx
+++ lib/components/AddRecipeForm/index.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import clsx from "clsx";
-import Input from "@ui/Input";
 import { Slider } from "@ui/slider";
+import { Input } from "@ui/input";
+import { Label } from "@ui/label";
 import Button from "@ui/Button";
 import type { Recipe } from "@prisma/client";
 
@@ -10,81 +12,127 @@ interface Props {}
 
 interface FormData extends Recipe {}
 
-const inputClasses = "border-solid border-2 p-2 rounded-md w-full mobile:w-3/5";
+const inputClasses =
+  "border-solid border-2 border-border p-2 rounded-md";
 const inputErrorClasses = "border-red-500 focus:outline-red-500";
 
 export default function AddRecipeForm({}: Props) {
   const {
     register,
     handleSubmit,
-    control,
+    watch,
+    getValues,
+    setValue,
     formState: { errors },
   } = useForm<FormData>({
     defaultValues: {
       title: "",
       cookTime: null,
       prepTime: null,
+      rating: 3,
     },
   });
 
+  const [ ratingDisplay, setRatingDisplay ] = useState<number>(3)
+
   const onSubmit = (data: any) => console.log(data);
 
-  //TODO Learn React Hook Form
 
   return (
     <>
+      <h2 className="text-2xl text-center">Add Recipe</h2>
       <form
-        className="flex flex-col items-center gap-4 outline outline-1"
+        className="flex flex-col justify-center items-center gap-4"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <input
-          className={clsx(inputClasses, errors.title && inputErrorClasses)}
-          id="title"
-          aria-invalid={!!errors.title}
-          {...register("title", { required: "Required" })}
-          placeholder="Recipe Name"
-        />
-        {errors.title?.message && (
-          <span role="alert" className="text-red-500">{errors.title.message}</span>
-        )}
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="title">Recipe Name</Label>
+          <Input
+            className={clsx(inputClasses, errors.title && inputErrorClasses)}
+            id="title"
+            aria-invalid={!!errors.title}
+            {...register("title", { required: "Required" })}
+          />
+          {errors.title?.message && (
+            <span role="alert" className="text-red-500">
+              {errors.title.message}
+            </span>
+          )}
+        </div>
 
-        <input
-          type="number"
-          id="prep-time"
-          aria-invalid={!!errors.prepTime}
-          className={clsx(inputClasses, errors.prepTime && inputErrorClasses)}
-          {...register("prepTime", {
-            required: "Required",
-            maxLength: {
-              value: 3,
-              message: "Cook time must be 3 digits or less",
-            },
-          })}
-          placeholder="Prep Time"
-        />
-        {errors.prepTime?.message && (
-          <span role="alert" className="text-red-500">{errors.prepTime.message}</span>
-        )}
+        <div className="flex flex-row w-3/4 justify-evenly gap-4">
+          <div className="grid w-1/2 mobile:w-1/3 max-w-sm items-center gap-1.5">
+            <Label htmlFor="prep-time">Prep Time</Label>
+            <Input
+              type="number"
+              id="prep-time"
+              aria-invalid={!!errors.prepTime}
+              className={clsx(
+                inputClasses,
+                errors.prepTime && inputErrorClasses,
+                'max-w-[4rem]'
+              )}
+              {...register("prepTime", {
+                required: "Required",
+                maxLength: {
+                  value: 3,
+                  message: "Cook time must be 3 digits or less",
+                },
+              })}
+            />
+            {errors.prepTime?.message && (
+              <span role="alert" className="text-red-500">
+                {errors.prepTime.message}
+              </span>
+            )}
+          </div>
 
-        <input
-          type="number"
-          id="cook-time"
-          aria-invalid={!!errors.cookTime}
-          className={clsx(inputClasses, errors.cookTime && inputErrorClasses)}
-          {...register("cookTime", {
-            required: "Required",
-            maxLength: {
-              value: 3,
-              message: "Cook time must be 3 digits or less",
-            },
-          })}
-          placeholder="Cook Time"
-        />
-        {errors.cookTime?.message && (
-          <span role="alert" className="text-red-500">{errors.cookTime.message}</span>
-        )}
+          <div className="grid w-1/2 mobile:w-1/3 max-w-sm items-center gap-1.5">
+            <Label htmlFor="cook-time">Cook Time</Label>
+            <Input
+              type="number"
+              id="cook-time"
+              aria-invalid={!!errors.cookTime}
+              className={clsx(
+                inputClasses,
+                errors.cookTime && inputErrorClasses,
+                'max-w-[4rem]'
+              )}
+              {...register("cookTime", {
+                required: "Required",
+                maxLength: {
+                  value: 3,
+                  message: "Cook time must be 3 digits or less",
+                },
+              })}
+            />
+            {errors.cookTime?.message && (
+              <span role="alert" className="text-red-500">
+                {errors.cookTime.message}
+              </span>
+            )}
+          </div>
+        </div>
 
-        <Slider />
+        <div className="flex flex-col mobile:flex-row w-full justify-center items-center gap-4">
+          <Label htmlFor="rating">Rating</Label>
+          <Slider
+            {...register("rating")}
+            id="rating"
+            className="w-full mobile:w-1/2 hover:cursor-pointer"
+            defaultValue={[3]}
+            min={1}
+            max={5}
+            step={1}
+            onValueChange={([e]) => {
+              setValue("rating", e);
+              setRatingDisplay(e);
+            }}
+          />
+          <span className="w-2">
+          {ratingDisplay}
+          </span>
+        </div>
 
         <Button type="submit">Submit</Button>
       </form>
