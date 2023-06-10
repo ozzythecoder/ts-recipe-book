9575b6ec71edac86984ab4cda4acc4ad25bb5b4c
August McAllister <august.chm@gmail.com>
2023-06-10 16:19:08 -0500

    Added disabled button background

--- lib/components/ui/Button/index.tsx
+++ lib/components/ui/Button/index.tsx
@@ -6,5 +6,5 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function Button({ className, children, ...props }: Props) {
 
-  return <button {...props} className={clsx(className, "h-10 px-4 rounded-md bg-blue-700 text-white")}>{children}</button>;
+  return <button {...props} className={clsx(className, "h-10 px-4 rounded-md bg-blue-700 text-white", props.disabled && "bg-gray-400")}>{children}</button>;
 }
