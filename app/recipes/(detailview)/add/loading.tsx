ffba4042169cb42521a789aece8e0f8bdb83dfe2
August McAllister <august.chm@gmail.com>
2023-06-21 16:07:31 -0500

    added Skeleton & loading page

---
+++ app/recipes/(detailview)/add/loading.tsx
@@ -0,0 +1,15 @@
+import { Skeleton } from "@ui/skeleton"
+
+export default function Loading() {
+
+  return (
+    <div className="flex flex-col gap-[1.5] justify-center items-center">
+      <Skeleton className="mb-2 h-12 w-[40%] bg-gray-200" />
+      <Skeleton className="mb-4 h-8 w-full max-w-sm bg-gray-200" />
+      <Skeleton className="mb-4 h-8 w-full max-w-sm bg-gray-200" />
+      <Skeleton className="mb-4 h-8 w-full max-w-sm bg-gray-200" />
+      <Skeleton className="mb-4 h-8 w-full max-w-sm bg-gray-200" />
+    </div>
+  )
+
+}
