d87e9f827e4b311a9465ebc7d54789f43c42d036
August McAllister <august.chm@gmail.com>
2023-06-21 13:26:07 -0500

    added arrow icon to "go back" link

--- app/recipes/(detailview)/layout.tsx
+++ app/recipes/(detailview)/layout.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 interface Props extends React.PropsWithChildren {}
 
 export default function RecipeLayout({ children }: Props) {
   return (
     <>
-    <div className="my-2">
-      <Link href="/recipes" className="text-sm text-gray-400 hover:text-black hover:underline focus:underline">Back to recipes</Link>
-    </div>
+      <span>
+        <Link
+          href="/recipes"
+          className="flex flex-row my-2 items-center justify-center mobile:justify-start text-sm text-gray-400 hover:text-black hover:underline focus:underline"
+        >
+          <ArrowLeft
+            className="inline mr-2"
+            size={14}
+          />
+          Back to recipes
+        </Link>
+      </span>
       <>{children}</>
     </>
   );
