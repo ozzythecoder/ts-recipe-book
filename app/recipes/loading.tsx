a605d0515250a00c718d50d6cb0a426c933a9d47
August McAllister <august.chm@gmail.com>
2023-06-21 16:07:45 -0500

    updated existing loading page

--- app/recipes/loading.tsx
+++ app/recipes/loading.tsx
@@ -1,9 +1,17 @@
+import { Skeleton } from "@ui/skeleton"
+
 export default function Loading() {
 
   return (
-    <h1 className="text-3xl">
-      Loading...
-    </h1>
+    <div>
+      <Skeleton className="h-12 w-full bg-gray-200" />
+      <div className="grid grid-cols-1 mobile:grid-cols-2 lg:grid-cols-3 gap-2 justify-evenly align-middle mt-4">
+        <Skeleton className="aspect-square bg-gray-200" />
+        <Skeleton className="aspect-square bg-gray-200" />
+        <Skeleton className="aspect-square bg-gray-200" />
+        <Skeleton className="aspect-square bg-gray-200" />
+      </div>
+    </div>
   )
 
 }
