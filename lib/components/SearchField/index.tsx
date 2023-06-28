6c54d161ef85261717a82b88428ac92e81e27a19
August McAllister <august.chm@gmail.com>
2023-06-28 14:28:44 -0500

    Added keyboard shortcut for search

--- lib/components/SearchField/index.tsx
+++ lib/components/SearchField/index.tsx
@@ -1,15 +1,32 @@
 "use client";
 import { useSearchContext } from "@/lib/context/search";
+import { useEffect, useRef } from "react";
 
 const SearchField = () => {
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
   const { recipeSearchIn, setRecipeSearch } = useSearchContext();
 
-  const handleChange = (e: React.SyntheticEvent) => {
-    setRecipeSearch!((e.target as HTMLInputElement).value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRecipeSearch!(e.target.value);
   };
+
+  // PRESS "/" TO SEARCH
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        inputRef.current?.focus();
+      }
+    }
+    document.addEventListener('keyup', handleKeyPress)
+    return () => document.removeEventListener('keyup', handleKeyPress)
+  }, [])
 
   return (
     <input
+      aria-label="Search recipes"
+      ref={inputRef}
       className="p-2 w-3/5 border-2 rounded-sm"
       type="text"
       value={recipeSearchIn}
