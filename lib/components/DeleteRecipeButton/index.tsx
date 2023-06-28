0138a964532dd42b863322e3ed4ce0fc69ec5a48
August McAllister <august.chm@gmail.com>
2023-06-28 10:56:44 -0500

    added loading state and delete logic to delete recipe button

--- lib/components/DeleteRecipeButton/index.tsx
+++ lib/components/DeleteRecipeButton/index.tsx
@@ -1,26 +1,42 @@
-"use client"
+// TODO:
+// * Confirm delete
+
+"use client";
 
 import { Button } from "@ui/button";
-import clsx from "clsx";
-import { Trash } from "lucide-react";
+import { Loader2, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
   className: string;
   id: number;
+  title: string;
 }
 
-export default function DeleteRecipeButton({ id, className }: Props) {
-
+export default function DeleteRecipeButton({ id, className, title }: Props) {
   const router = useRouter();
 
-  const handleDelete = () => {
-    alert('Delete clicked');
-  }
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleDelete = async () => {
+    setLoading(true);
+
+    const response = await fetch(`http://localhost:3000/api/recipe/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      router.push("/recipes");
+    } else {
+      setLoading(false);
+      console.log(response.statusText);
+    }
+  };
 
   return (
-    <Button variant="destructive" type="button" className={clsx(className, "p-4")} onClick={handleDelete}>
-      <Trash className="h-4 w-4" />
+    <Button disabled={loading} aria-label={`Delete recipe ${title}`} variant="destructive" type="button" size="sm" className={className} onClick={handleDelete}>
+      {loading ? <Loader2 className="h-4 w-4 animate-spin" /> : <Trash className="h-4 w-4" />}
     </Button>
-  )
+  );
 }
